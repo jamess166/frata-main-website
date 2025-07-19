acdb31171115ecb8534993d1fd044a3b08cc0b9c
James Cabrera <jamescabrerabr@gmail.com>
2025-07-19 15:42:57 +0000

    AHORA SI, DESARROLLAME EL MECANISMO PARA LA DESCRIPCION DE CADA ADDIN, S

--- src/app/manual/page.tsx
+++ src/app/manual/page.tsx
@@ -61,6 +61,7 @@ function ManualContentDisplay({ addin }: { addin: Addin }) {
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
+              className="w-full h-full"
             ></iframe>
           </div>
         </div>
